787d6c6c431f015a65afbe376433d380d036936c
Paul Maxson <paul.f.maxson@gmail.com>
2019-01-23 15:38:55 -0500

    Simplify DOM Ref implementation

--- src/Presentational/Components/AlarmAudio.js
+++ src/Presentational/Components/AlarmAudio.js
@@ -3,60 +3,20 @@ import React, {
   useContext,
   useMemo,
   createRef,
-  useEffect,
 } from 'react';
 
 import { ClockMachineServiceContext } from '../../';
 
 import { useServiceForContext } from '../../UseMachine';
 
-// const updateAudioPlayingStatus = async (audio, shouldBePlaying) => {
-//   if (audio !== null) {
-//     if (audio.paused) {
-//       if (shouldBePlaying) return await audio.play();
-//     } else if (!shouldBePlaying) return audio.pause();
-//   }
-// };
-//
-// const updateAudioVolume = (audio, volume) => {
-//   if (audio !== null) {
-//     audio.volume = volume;
-//   }
-// };
-//
-// const Audio = ({ src, volume, shouldBePlaying }) => {
-//   const ref = createRef();
-//   console.log(ref);
-//   useEffect(
-//     () => {
-//       updateAudioPlayingStatus(ref.current, shouldBePlaying);
-//     },
-//     [ref.current, shouldBePlaying]
-//   );
-//
-//   useEffect(() => updateAudioVolume(ref.current, volume), [
-//     ref.current,
-//     volume,
-//   ]);
-//
-//   return (
-//     <audio src={src} ref={ref}>
-//       Your browser does not support the
-//       <code>audio</code> element.
-//     </audio>
-//   );
-// };
-
-function getAudioRef() {
-  return this.audioRef;
-}
+// Clamp volume btw 0 and 1
+const sanitizeVolume = volume => Math.min(Math.max(volume, 0), 1);
 
 class Audio extends Component {
   // TODO: Add volume control functionality
   constructor(props) {
     super(props);
-    this.setAudioRef = this.setAudioRef.bind(this);
-    getAudioRef = getAudioRef.bind(this);
+    this.audioRef = createRef();
   }
 
   shouldComponentUpdate(nextProps) {
@@ -66,43 +26,23 @@ class Audio extends Component {
   componentDidUpdate(prevProps) {
     const newProps = this.props;
     console.debug({ desc: 'start of cdu', prevProps, newProps });
-    if (this.props.shouldBePlaying)
-      this.audioRef
+    if (this.props.shouldBePlaying) 
+      this.audioRef.current
         .play()
-        .then(result =>
-          console.debug({
-            desc: 'play succeeded',
-            result,
-            audioRef: getAudioRef(),
-          })
-        )
-        .catch(err => console.debug({ desc: 'play failed', err }));
-    else this.audioRef.pause();
+        .catch(err => console.error({ desc: 'play failed', err }));
+    else this.audioRef.current.pause();
     const { audioRef } = this;
     console.debug({ desc: 'end of cdu', audioRef });
   }
 
-  setAudioRef(audioRef) {
-    this.audioRef = audioRef;
-    console.debug({
-      desc: 'setAudioRef',
-      play: audioRef.play,
-      pause: audioRef.pause,
-      paused: audioRef.paused,
-    });
-  }
-
   render() {
     return (
-      <audio src={this.props.src} ref={this.setAudioRef} preload="auto" loop>
+      <audio src={this.props.src} ref={this.audioRef} preload="auto" loop>
         Your browser does not support the <code>audio</code> element.
       </audio>
     );
   }
 }
-
-// Clamp volume btw 0 and 1
-const sanitizeVolume = volume => Math.min(Math.max(volume, 0), 1);
 
 export default function({ src, volume = 1 }) {
   const validVolume = useMemo(() => sanitizeVolume(volume), [volume]);
